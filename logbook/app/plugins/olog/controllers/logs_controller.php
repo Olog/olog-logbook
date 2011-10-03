455df092ef2126dd4ce32fe0407eba7b13fd3139
gaul <gaul@gaul-dev.intranet.nscl.msu.edu>
2011-10-03 14:31:37 -0400

    logs_controller - refactor and reformat

--- logbook/app/plugins/olog/controllers/logs_controller.php
+++ logbook/app/plugins/olog/controllers/logs_controller.php
@@ -17,31 +17,32 @@ class LogsController extends OlogAppController {
         );
         $this->set('logs', $this->paginate('Log'));
 
-        $levels = array("Info" => "Info",
-            "Problem" => "Problem",
-            "Request" => "Request",
-            "Suggestion" => "Suggestion",
-            "Urgent" => "Urgent");
+        Controller::loadModel('Level');
+        $levelsInDb = $this->Level->find('list');
+        foreach ($levelsInDb as $level) {
+            $levels[$level] = $level;
+        }
+        $this->set('levels', $levels);
 
         Controller::loadModel('Logbook');
         $logbooks = $this->Logbook->find('list');
+        $this->set('logbooks', $logbooks);
 
         Controller::loadModel('Tag');
         $tags = $this->Tag->find('list');
-
-        $this->set('logbooks', $logbooks);
-        $this->set(compact('tags', 'levels'));
-
-        $this->set('session', $this->Session);
-        $this->set('base', $this->base);
 
         $argumentString = '';
         foreach ($this->passedArgs as $argumentName => $argumentValue) {
             $argumentString .= '/' . $argumentName . ':' . $argumentValue;
         }
-        $this->set(compact('argumentString'));
+
+        $this->set('session', $this->Session);
+        $this->set('base', $this->base);
+
+        $this->set(compact('tags', 'argumentString'));
     }
 
+    // TODO - Determine whether this action and its associated view is necessary. It looks like it currently isn't being used.
     function view($id = null) {
         if (!$id) {
             $this->Session->setFlash(__('Invalid log', true));
@@ -51,19 +52,18 @@ class LogsController extends OlogAppController {
     }
 
     function add() {
-        if (!empty($this->data)) {
 
+        if (!empty($this->data)) {
             if (isset($this->data['log']['username']) && isset($this->data['log']['password'])) {
                 $this->Log->request['auth']['user'] = $this->data['log']['username'];
                 $this->Log->request['auth']['pass'] = $this->data['log']['password'];
             }
 
-            $saved = $this->Log->save($this->data);
-            if ($saved) {
+            if ($this->Log->save($this->data)) {
                 $this->Session->setFlash(__('The log has been saved', true));
                 $this->redirect(array('action' => 'index'));
             } else {
-                /** Todo quick fix for tag validation error not showing up * */
+                // TODO - quick fix for tag validation error not showing up
                 $print_error = "";
                 foreach ($this->Log->validationErrors as $errorKey => $error) {
                     $print_error .= "For input " . $errorKey . ": " . $error . '<br>';
@@ -72,48 +72,6 @@ class LogsController extends OlogAppController {
                 $this->redirect(array('action' => 'index'));
             }
         }
-        $levels = array("Info" => "Info",
-            "Problem" => "Problem",
-            "Request" => "Request",
-            "Suggestion" => "Suggestion",
-            "Urgent" => "Urgent");
-        Controller::loadModel('Tag');
-        $tags = $this->Tag->find('list');
-        Controller::loadModel('Logbook');
-        $logbooks = $this->Logbook->find('list');
-        //$uploads = $this->Log->Upload->find('list');
-        $this->set(compact('levels', 'tags', 'logbooks'));
-    }
-    
-    function addproperty(){
-	$this->autoRender = false;
-
-        //if (!empty($this->data)) {
-
-            $logProperty = array();
-
-            foreach($this->params['form'] as $key => $value){
-                if ($key == 'logId'){
-                    $logProperty['log']['id']=$value;
-                    $dbinfo = get_class_vars('DATABASE_CONFIG');
-                    $logProperty['log']['subject']=$dbinfo['olog']['default_subject'];
-                } else {
-                    $logProperty['log']['properties'][str_replace('_', '.', $key)]=$value;
-                }
-            }
-
-            $saved = $this->Log->merge($logProperty);
-            if ($saved) {
-                return 'The property has been saved';
-            } else {
-                /** Todo quick fix for tag validation error not showing up * */
-                $print_error = "";
-                foreach ($this->Log->validationErrors as $errorKey => $error) {
-                    $print_error .= "For input " . $errorKey . ": " . $error . '<br>';
-                }
-                return $print_error . 'The log could not be saved. Please, try again.';
-            }
-        //}
     }
 
     function edit($id = null) {
@@ -121,14 +79,16 @@ class LogsController extends OlogAppController {
             $this->Session->setFlash(__('Invalid log', true));
             $this->redirect(array('action' => 'index'));
         }
+
         if (!empty($this->data)) {
+
             // Note: Passing 'key->value' as option values, b/c I can't pass keys in a select
             $properties = array();
-            foreach($this->data['log']['properties'] as $property){
-                list($key,$value)=explode('->',$property);
-                $properties[$key]=$value;
+            foreach ($this->data['log']['properties'] as $property) {
+                list($key, $value) = explode('->', $property);
+                $properties[$key] = $value;
             }
-            $this->data['log']['properties']=$properties;
+            $this->data['log']['properties'] = $properties;
 
             if ($this->Log->save($this->data)) {
                 $this->Session->setFlash(__('The log has been saved', true));
@@ -136,19 +96,17 @@ class LogsController extends OlogAppController {
             } else {
                 $this->Session->setFlash(__('The log could not be saved. Please, try again.', true));
             }
-
-        }
-        if (empty($this->data)) {
+        } else {
             $this->data = $this->Log->find('log', array('conditions' => array('id' => $id)));
         }
 
         Controller::loadModel('Tag');
         $tags = $this->Tag->find('list');
+
         Controller::loadModel('Logbook');
         $logbooks = $this->Logbook->find('list');
- 
-        $this->set(compact('levels', 'tags', 'logbooks'));
 
+        $this->set(compact('levels', 'tags', 'logbooks'));
     }
 
     function delete($id = null) {
@@ -159,25 +117,67 @@ class LogsController extends OlogAppController {
         if ($this->Log->delete($id)) {
             $this->Session->setFlash(__('Log deleted', true));
             $this->redirect(array('action' => 'index'));
+        } else {
+            $this->Session->setFlash(__('Log was not deleted', true));
+            $this->redirect(array('action' => 'index'));
         }
-        $this->Session->setFlash(__('Log was not deleted', true));
-        $this->redirect(array('action' => 'index'));
     }
 
+    /**
+     * Adds an IRMIS property to a log entry. Parameters are sent implicitly.
+     * 
+     * @return $flashMessage Success or failure message 
+     */
+    function addproperty() {
+        $this->autoRender = false;
+
+        $flashMessage = "";
+
+        $logProperty = array();
+
+        foreach ($this->params['form'] as $key => $value) {
+            if ($key == 'logId') {
+                $logProperty['log']['id'] = $value;
+                $dbinfo = get_class_vars('DATABASE_CONFIG');
+                $logProperty['log']['subject'] = $dbinfo['olog']['default_subject'];
+            } else {
+                $logProperty['log']['properties'][str_replace('_', '.', $key)] = $value;
+            }
+        }
+
+        $saved = $this->Log->merge($logProperty);
+        if ($saved) {
+            $flashMessage = 'The property has been saved';
+        } else {
+            // TODO - quick fix for tag validation error not showing up
+            foreach ($this->Log->validationErrors as $errorKey => $error) {
+                $flashMessage .= "For input " . $errorKey . ": " . $error . '<br>';
+            }
+            $flashMessage . "The log could not be saved. Please, try again.";
+        }
+
+        return $flashMessage;
+    }
+
+    /**
+     * Pass any timespan change arguments from the dropdown menu to the index page.
+     * 
+     * @param $newTimeSpan Incoming timespan selection
+     */
     function timespanChange($newTimeSpan = null) {
         $startDate = null;
         $endDate = date('U');
-        
+
         $argumentString = '';
         foreach ($this->passedArgs as $argumentName => $argumentValue) {
             if ($argumentName != '0' && $argumentName != 'start' && $argumentName != 'end') {
                 $argumentString .= '/' . $argumentName . ':' . $argumentValue;
             }
         }
-        
+
         switch ($newTimeSpan) {
             case 0:
-                $this->redirect('/olog/logs/index/'.$argumentString);
+                $this->redirect('/olog/logs/index/' . $argumentString);
             case 1: // Last day
                 $startDate = mktime(0, 0, 0, date('m'), date('d') - 1, date('y'));
                 break;
@@ -203,7 +203,7 @@ class LogsController extends OlogAppController {
 
         $this->redirect('/olog/logs/index/start:' . $startDate . '/end:' . $endDate . $argumentString);
     }
-    
+
 }
 
 ?>
