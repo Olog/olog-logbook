40a7909ce8623847820461de2bcdbc697701048e
gaul <gaul@gaul-dev.intranet.nscl.msu.edu>
2011-10-03 14:32:14 -0400

    users_controller - refactor and reformat

--- logbook/app/plugins/olog/controllers/users_controller.php
+++ logbook/app/plugins/olog/controllers/users_controller.php
@@ -6,13 +6,19 @@ class UsersController extends OlogAppController {
     var $uses = array();
     var $helpers = array('Html', 'Form', 'Session');
 
+    /**
+     * Login action.
+     */
     function login() {
         if ($this->Session->read('LogAuth.User')) {
             $this->Session->setFlash('You are logged in!');
             $this->redirect('/', null, false);
-        } 
+        }
     }
 
+    /**
+     * Logout action.
+     */
     function logout() {
         $this->Session->setFlash('Good-Bye');
         $this->redirect($this->LogAuth->logout());
