7d6db969ae6b29d274a5ba09869bd64696ebdb06
berryman <eric.berryman@gmail.com>
2013-02-26 14:35:39 -0500

    changed version number for patch release

--- logbook/app/config/database.php
+++ logbook/app/config/database.php
@@ -100,7 +100,7 @@ class DATABASE_CONFIG {
 		'port' => '8181',
 		'path' => 'Olog/resources',
                 'default_subject' => 'Logbook Application',
-                'version' => 'v2.2.2'
+                'version' => 'v2.2.2-1'
 	);
 	
 	var $irmis = array(
